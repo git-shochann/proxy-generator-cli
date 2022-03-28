572a0297a6fcc6ba45cab45bb0f10e1cf7c04034
git-shochann <sho_825.sa216@docomo.ne.jp>
2022-03-29 01:50:10 +0900

    ✨: add func to make instance(wip), update memo

--- api/instance.go
+++ api/instance.go
@@ -1,7 +1,14 @@
 package api
 
+import "fmt"
+
 // インスタンス作成 リクエスト時に必要なデータ
 type Instance struct {
 	Imageid        string // CentOS 7.8
 	Instancetypeid string // t2
+}
+
+func CreateInstance(token string, tenantid string) {
+	endpoint := "https://jp1-api-instance.infrastructure.cloud.toast.com" + "/v2/" + tenantid + "/servers"
+	fmt.Println(endpoint)
 }
--- main.go
+++ main.go
@@ -8,4 +8,5 @@ import (
 func main() {
 	token := api.GetToken()
 	fmt.Println(token.Access.Token.ID) // データが格納されている構造体にアクセス
+	api.CreateInstance(token.Access.Token.ID, token.Access.Token.Tenant.ID)
 }
