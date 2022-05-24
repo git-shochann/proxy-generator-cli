df135069ab573ae1047e7747dbbf13a2264b029d
git-shochann <sho_825.sa216@docomo.ne.jp>
2022-05-24 23:58:07 +0900

    wip

--- api/token.go
+++ api/token.go
@@ -3,11 +3,10 @@ package api
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
-	"os"
 	"time"
 
 	"gopkg.in/ini.v1"
@@ -15,11 +14,97 @@ import (
 
 var TokenEndpoint = "https://api-identity.infrastructure.cloud.toast.com/v2.0/tokens"
 
+// グローバル変数として全スコープの範囲にて型を宣言 この変数Bodyをmain関数で使用するため。
+var Body RequestBody
+
+// 初めにconfigを読み込む
+func LoadConfig() err {
+
+	cfg, err := ini.Load("config.ini")
+	if err != nil {
+		return nil, getErrorResponse(err)
+	}
+
+	// 宣言した空の変数Bodyを初期化する
+	Body = RequestBody{
+		Auth: Data{
+			Tenantid: cfg.Section("toast").Key("tenantid").String(),
+			Passwordcredentials: Passwordcredentials{
+				Username: cfg.Section("toast").Key("username").String(),
+				Password: cfg.Section("toast").Key("password").String(),
+			},
+		},
+	}
+
+	return nil
+}
+
+// 戻り値で*Token型のstructとerrorを返す。
+
+func GetToken() (*Token, error) {
+
+	err := LoadConfig()
+	if err != nil {
+		return nil, getErrorResponse(err)
+	}
+
+	// fmt.Println(Body.Auth.Passwordcredentials.Username)
+	encodedjson, err := json.Marshal(Body)
+	if err != nil {
+		return nil, getErrorResponse(err)
+	}
+
+	// リクエストの作成
+	req, err := http.NewRequest("POST", TokenEndpoint, bytes.NewBuffer(encodedjson))
+	if err != nil {
+		return nil, getErrorResponse(err)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	client := http.Client{}
+	res, err := client.Do(req)
+
+	if err != nil {
+		return nil, getErrorResponse(err)
+	}
+
+	defer res.Body.Close()
+
+	data, err := ioutil.ReadAll(res.Body)
+
+	if err != nil {
+		return nil, getErrorResponse(err)
+	}
+
+	// レスポンスのstructの初期化
+	var token *Token
+
+	// 関数にアドレスを渡して直接操作できるようにする(実際にデータを参照して、変更を加える)
+	err = json.Unmarshal(data, &token)
+
+	if err != nil {
+		return nil, getErrorResponse(err)
+	}
+
+	// 上記で token *tokenのポインタ型と宣言しているのでここの返り値の設定は、tokenのみでOK
+	fmt.Printf("%T, %v", token, token)
+	return token, nil
+
+}
+
+func getErrorResponse(message string) error {
+	return errors.New(message)
+}
+
+/* リクエストボディで必要なstruct */
+
 type RequestBody struct {
 	Auth Data `json:"auth"`
 }
+
 type Data struct {
-	Tenantid            string              `json:"tenantId"` // タグに空白入れるとエラー発生する
+	Tenantid            string              `json:"tenantId"`
 	Passwordcredentials Passwordcredentials `json:"passwordCredentials"`
 }
 
@@ -28,7 +113,9 @@ type Passwordcredentials struct {
 	Password string `json:"password"`
 }
 
-// JSONデコード用の構造体 グローバルで使用できるようにする
+/* レスポンスで必要なstruct */
+// TODO: structの見直しを行う
+
 type Token struct {
 	Access struct {
 		Token struct {
@@ -67,77 +154,4 @@ type Token struct {
 			IsAdmin int      `json:"is_admin"`
 		} `json:"metadata"`
 	} `json:"access"`
-}
-
-var Body RequestBody // 全スコープ対象に型を宣言 = グローバル変数 // 実際にインスタンス化したBodyをmain関数で使用するため。
-
-func init() {
-
-	cfg, err := ini.Load("config.ini")
-	if err != nil {
-		fmt.Printf("fail to load config file: %v", err)
-		os.Exit(1)
-	}
-
-	Body = RequestBody{
-		Auth: Data{
-			Tenantid: cfg.Section("toast").Key("tenantid").String(),
-			Passwordcredentials: Passwordcredentials{
-				Username: cfg.Section("toast").Key("username").String(),
-				Password: cfg.Section("toast").Key("password").String(),
-			},
-		},
-	}
-}
-
-// ポインタ型を返す あくまでも戻り値の型。実際のデータではない。
-func GetToken() Token {
-
-	// fmt.Println(Body.Auth.Passwordcredentials.Username)
-	encodedjson, err := json.Marshal(Body) // JSONに変換
-	if err != nil {
-		fmt.Printf("fail to encode json: %v", err)
-		os.Exit(1)
-	}
-
-	// fmt.Println(string(encodedjson))
-
-	req, err := http.NewRequest("POST", TokenEndpoint, bytes.NewBuffer(encodedjson)) // リクエストの作成
-	if err != nil {
-		fmt.Printf("err")
-		os.Exit(1)
-	}
-
-	req.Header.Set("Content-Type", "application/json")
-
-	client := http.Client{}
-	res, err := client.Do(req)
-
-	if err != nil {
-		fmt.Printf("err")
-		os.Exit(1)
-	}
-
-	defer res.Body.Close()
-
-	data, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	// fmt.Println(string(data))
-
-	// レスポンスのJSONデータを、構造体にマッピングして外部から参照できるようにする
-
-	var token Token // 構造体の初期化
-
-	err = json.Unmarshal(data, &token) // 関数にアドレスを渡して直接操作できるようにする(実際にデータを参照して、変更を加える)
-
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	return token // データのアドレスを返す これは型ではないのでポインタ型である*ではない。 エラーメッセージ cannot use token (variable of type Token) as *Token value in return statement は正しい。
-
-	// ここの変数はそのままかアドレスどっちがいいのか？
 }
---
+++ memo.md
@@ -0,0 +1,21 @@
+## memo
+
+### CentOS 7.8
+
+ae0b0150-fd2e-411e-8c41-4f22b371ef81
+
+### u2.c1m1
+
+b41750b4-d819-487d-84bc-89fc7a6d0df1
+
+### t2.c1m1
+
+2718e9c1-b887-460b-bf4e-abcc2b010ec6
+
+---
+
+1,
+token.go -> API 呼び出し時に必要なトークンを発行。
+instance.go -> 1 個のインスタンスを作成する。
+floating_ip.go -> グローバル IP をアタッチする。
+proxy.sh -> サーバーに ssh 接続を行い、Proxy サーバーを構築する。
