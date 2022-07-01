fc6123c1f745a3596339818a3919e120d0381198
git-shochann <sho_825.sa216@docomo.ne.jp>
2022-07-01 17:32:43 +0900

    fix bug, rafactor code

--- internal/instance.go
+++ internal/instance.go
@@ -11,10 +11,11 @@ import (
 )
 
 const (
-	imageid         = "ae0b0150-fd2e-411e-8c41-4f22b371ef81" // centos
-	u2              = "b41750b4-d819-487d-84bc-89fc7a6d0df1"
-	t2              = "2718e9c1-b887-460b-bf4e-abcc2b010ec6" // t2を使用する場合リクエストの内容が変わる
-	subnetid string = "b9196e60-934c-40ea-af80-f5c7e991d3fd"
+	imageID    string = "ae0b0150-fd2e-411e-8c41-4f22b371ef81" // centos
+	u2Instance string = "b41750b4-d819-487d-84bc-89fc7a6d0df1"
+	t2instance string = "2718e9c1-b887-460b-bf4e-abcc2b010ec6" // t2を使用する場合リクエストの内容が変わる
+	subnetID   string = "b9196e60-934c-40ea-af80-f5c7e991d3fd"
+	baseURL    string = "https://jp1-api-instance.infrastructure.cloud.toast.com"
 )
 
 func Createinstance(token string, tenantid string) (*ResponseInstanse, error) {
@@ -22,41 +23,25 @@ func Createinstance(token string, tenantid string) (*ResponseInstanse, error) {
 	// サーバーネームをランダムで出力する
 	randomName := pkg.RandomGenerate(10)
 
-	// リクエストをするための構造体の初期化
+	// リクエストをするための構造体の初期化 + インスタンス化
+
 	requestBody := RequestInstanse{
 		Server: Server{
 			Name:      randomName,
-			ImageRef:  imageid,
-			FlavorRef: u2,
-			Network: []Networks{{
-				Subnet: subnetid,
-			}},
-			Keyname: config.Config.KeyName,
+			ImageRef:  imageID,
+			FlavorRef: u2Instance,
+			NetWork: []NetWork{
+				{Subnet: subnetID},
+			},
+			KeyName: config.Config.KeyName,
 		},
 	}
 
-	// Request
-	type RequestInstanse struct {
-		Server `json:"server"`
-	}
-
-	type Server struct {
-		Name      string     `json:"name"`
-		ImageRef  string     `json:"imageRef"`
-		FlavorRef string     `json:"flavorRef"`
-		Network   []Networks `json:"networks"`
-		Keyname   string     `json:"key_name"`
-	}
-
-	type Networks struct {
-		Subnet string `json:"subnet"`
-	}
-
-	endpoint := "https://jp1-api-instance.infrastructure.cloud.toast.com" + "/v2/" + tenantid + "/servers"
+	endpoint := baseURL + "/v2/" + tenantid + "/servers"
 
 	encodedjson, err := json.Marshal(requestBody)
 	if err != nil {
-		log.Fatalln(err)
+		log.Println(err)
 	}
 
 	req, err := http.NewRequest("POST", endpoint, bytes.NewBuffer(encodedjson))
@@ -103,14 +88,14 @@ type RequestInstanse struct {
 }
 
 type Server struct {
-	Name      string     `json:"name"`
-	ImageRef  string     `json:"imageRef"`
-	FlavorRef string     `json:"flavorRef"`
-	Network   []Networks `json:"networks"`
-	Keyname   string     `json:"key_name"`
+	Name      string    `json:"name"`
+	ImageRef  string    `json:"imageRef"`
+	FlavorRef string    `json:"flavorRef"`
+	NetWork   []NetWork `json:"networks"`
+	KeyName   string    `json:"key_name"`
 }
 
-type Networks struct {
+type NetWork struct {
 	Subnet string `json:"subnet"`
 }
 
