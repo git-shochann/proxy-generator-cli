4fa4bdb93088df00424d6b3d7a0a6df40420090a
git-shochann <sho_825.sa216@docomo.ne.jp>
2022-07-11 23:58:59 +0900

    delete temp

--- main.go
+++ main.go
@@ -1,11 +1,5 @@
-/*
-Copyright © 2022 NAME HERE <EMAIL ADDRESS>
-*/
-
 package main
 
-import "nhn-toast-api/cmd"
-
 func main() {
-	cmd.Execute()
+
 }
---
+++ tsuboya/main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2022 sho
+
+*/
+package main
+
+import "nhn-toast-api/cmd"
+
+func main() {
+	cmd.Execute()
+}
